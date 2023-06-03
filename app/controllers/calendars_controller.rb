3236326d3b60d483d6ce10554d04682320b29fc1
nakaken0918 <nakaken18.wywy@gmail.com>
2023-06-03 23:22:12 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,15 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      # 今日の日付を取得し、+ x を追加。timesの実行毎に + 1 される。
+      wday_num = Date.today.day + x
+      # 中身の値を 0 ~ 6 のローテーションにしたいので、7 以上の場合に -7 を実行。
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+      # 中身の値を曜日で表記したいので、wday_numに対応したwdaysを取り出すよう記述。wdays[wday_num]
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      
       @week_days.push(days)
     end
 
